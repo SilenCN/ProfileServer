f1c07e8412d64da13af7eccb3991d76318a6e455
SilenCN <1039795376@qq.com>
2017-03-05 10:46:57 +0800

    Delete SQLInit.java

--- src/com/xiaoxing/SQLInit.java
+++
@@ -1,8 +0,0 @@
-public class SQLInit {
-    public static final String DatabaseServer = "localhost";
-    public static final int DatabasePort = 3306;
-    public static final String DatabaseUser = "root";
-    public static final String DatabasePW = "";//密码
-    public static final String DatabaseName = "Xiaoxing";
-    public static final String DatabaseDriver = "com.mysql.jdbc.Driver";
-}
