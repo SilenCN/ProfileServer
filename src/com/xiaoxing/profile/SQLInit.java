5b4d0659f222d33c82e576882ea492e33b75142e
SilenCN <1039795376@qq.com>
2017-03-05 10:48:03 +0800

    Create SQLInit.java

---
+++ src/com/xiaoxing/profile/SQLInit.java
@@ -0,0 +1,13 @@
+package com.xiaoxing.profile;
+
+/**
+ * Created by silen on 16-10-10.
+ */
+public class SQLInit {
+    public static final String DatabaseServer = "localhost";
+    public static final int DatabasePort = 3306;
+    public static final String DatabaseUser = "root";
+    public static final String DatabasePW = "";//密码
+    public static final String DatabaseName = "Xiaoxing";
+    public static final String DatabaseDriver = "com.mysql.jdbc.Driver";
+}
